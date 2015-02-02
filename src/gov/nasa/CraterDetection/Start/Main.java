4d7b4941f723a2141657baf5f581dc44c26bf51b
Mohammad Nassar <safeermohammed13190@yahoo.co.uk>
2015-02-02 22:38:55 +0000

    Committing the first project for the Nasa Crater Detection Algorithm.

---
+++ src/gov/nasa/CraterDetection/Start/Main.java
@@ -0,0 +1,10 @@
+package gov.nasa.CraterDetection.Start;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Starting Point
+		System.out.println("Working Successfully.....");
+	}
+
+}
